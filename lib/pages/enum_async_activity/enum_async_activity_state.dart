3823eb3d00f965ae1a228a0898aa71ec3ccfb865
heavyfran <heavyfran@gmail.com>
2024-11-08 15:54:19 +0900

    enumAsyncActivityProvider(lewagon)

---
+++ lib/pages/enum_async_activity/enum_async_activity_state.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../models/activity.dart';
+
+part 'enum_async_activity_state.freezed.dart';
+
+enum ActivityStatus {
+  loading,
+  success,
+  failure,
+}
+
+@freezed
+class EnumAsyncActivityState with _$EnumAsyncActivityState {
+  const factory EnumAsyncActivityState({
+    required ActivityStatus status,
+    required Activity activity,
+    required String error,
+  }) = _EnumAsyncActivityState;
+
+  factory EnumAsyncActivityState.initial() {
+    return EnumAsyncActivityState(
+      status: ActivityStatus.loading,
+      activity: Activity.empty(),
+      error: '',
+    );
+  }
+}
